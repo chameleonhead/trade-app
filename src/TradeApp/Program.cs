d78439452754fa69e6c827b6dd87fd4d329be167
Yuto Nagano <chamelion@hotmail.co.jp>
2017-11-09 22:33:07 +0900

    initial commit.

---
+++ src/TradeApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TradeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ tests/TradeApp.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TradeApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
