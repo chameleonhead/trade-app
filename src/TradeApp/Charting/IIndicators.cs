e7188ff5cd6239c983d5dd5e01afaad1c3d261dd
Yuto Nagano <chamelion@hotmail.co.jp>
2018-01-27 03:06:53 +0900

    単純移動平均計算器を作成

---
+++ src/TradeApp/Charting/IIndicators.cs
@@ -0,0 +1,7 @@
+﻿namespace TradeApp.Charting
+{
+    public interface IIndicator<in TIn, out TOut>
+    {
+        TOut NextValue(TIn data);
+    }
+}
---
+++ src/TradeApp/Charting/Indicators/SmaIndicator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace TradeApp.Charting.Indicators
+{
+    public class SmaIndicator : IIndicator<decimal, decimal?>
+    {
+        private Queue<decimal> samples;
+        private decimal total;
+        private int period;
+
+        public SmaIndicator(int period)
+        {
+            this.samples = new Queue<decimal>();
+            this.period = period;
+        }
+
+        public decimal? NextValue(decimal data)
+        {
+            total += data;
+            samples.Enqueue(data);
+
+            if (samples.Count <= period)
+            {
+                return total / samples.Count;
+            }
+
+            total -= samples.Dequeue();
+            return total / period;
+        }
+    }
+}
